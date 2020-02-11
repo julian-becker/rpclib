368f3707b4bb6cb468ff01daa693ad0fa2ec6f9a
Julian Becker <becker.julian@gmail.com>
2020-02-11 21:58:44 +0100

    ...

---
+++ rpc/include/rpc/server.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace rpc {
+
+class server {
+    int doo() { return 0; }
+
+public:
+    server() {}
+};
+
+}
--- rpc_test/src/test1.cpp
+++ rpc_test/src/test1.cpp
@@ -1,7 +1,9 @@
 #include <catch2/catch.hpp>
 #include <rpc/rpc.h>
+#include <rpc/server.h>
 
-TEST_CASE("rpc_api_foobar returns the ultimate result", "[rpc_api_foobar]")
+TEST_CASE("server can be instantiated", "[server_base_api]")
 {
-    REQUIRE(42 == rpc_api());
+    using namespace rpc;
+    REQUIRE_NOTHROW(server {});
 }
